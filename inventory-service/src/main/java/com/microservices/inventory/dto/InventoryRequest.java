a3c0ba322993a49e76be9b0c38f5a7b7867cbe5b
JohnUfo <muydinovumidjon17@gmail.com>
2024-11-12 15:43:05 +0500

    everything is working, test is working

---
+++ inventory-service/src/main/java/com/microservices/inventory/dto/InventoryRequest.java
@@ -0,0 +1,4 @@
+package com.microservices.inventory.dto;
+
+public record InventoryRequest() {
+}
---
+++ inventory-service/target/classes/db/migration/V2__inventory.sql
@@ -0,0 +1,8 @@
+insert into t_inventory(quantity,sku_code)
+values
+    (100,'iphone_14_pro'),
+    (100,'iphone_14_pro_max'),
+    (100,'iphone_15_pro'),
+    (100,'iphone_15_pro_max'),
+    (100,'iphone_16_pro'),
+    (100,'iphone_16_pro_max');
